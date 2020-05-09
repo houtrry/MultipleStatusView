7c2a313bf0f8e059328273351072bac979ba67f6
houtrry <houtrry@hotmail.com>
2020-05-09 18:15:38 +0800

    添加方法注释

--- multiplestatusview/src/main/java/com/github/multiplestatusview/IMultipleStatusView.java
+++ multiplestatusview/src/main/java/com/github/multiplestatusview/IMultipleStatusView.java
@@ -11,21 +11,59 @@ import androidx.annotation.NonNull;
  * @description:
  */
 public interface IMultipleStatusView {
+    /**
+     * 展示数据页
+     */
     void showDataView();
 
+    /**
+     * 展示loading(默认不会隐藏数据页)
+     */
     void showLoadingView();
 
+    /**
+     * 展示loading
+     *
+     * @param remainDataProgress 是否隐藏数据页
+     */
     void showLoadingView(boolean remainDataProgress);
 
+    /**
+     * 展示错误页
+     */
     void showErrorView();
 
+    /**
+     * 展示空数据页
+     */
     void showEmptyView();
 
+    /**
+     * 展示未登录页
+     */
     void showNotLoginView();
 
+    /**
+     * 设置错误页面的点击事件
+     *
+     * @param viewId   接收点击事件的View id
+     * @param listener
+     */
     void setOnErrorViewClickListener(int viewId, @NonNull View.OnClickListener listener);
 
+    /**
+     * 设置空数据页面的点击事件
+     *
+     * @param viewId   接收点击事件的View id
+     * @param listener
+     */
     void setOnEmptyViewClickListener(int viewId, @NonNull View.OnClickListener listener);
 
+    /**
+     * 设置未登录页面的点击事件
+     *
+     * @param viewId   接收点击事件的View id
+     * @param listener
+     */
     void setOnNotLoginViewClickListener(int viewId, @NonNull View.OnClickListener listener);
 }
